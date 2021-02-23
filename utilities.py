f2b092d3b526ceef56682000bd90746245c2a467
Lewis Moffat <lewis.moffat@cs.ucl.ac.uk>
2021-02-23 15:00:54 +0000

    First running commit

---
+++ utilities.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 14:55:56 2020
+
+@author:
+    Lewis Moffat
+    Bioinformatics Group - Comp. Sci. Dep., University College London (UCL)
+    Github: CraftyColossus
+
+"""
+
+
+def aas2int(seq):
+    aanumdict = {'A':0, 'R':1, 'N':2, 'D':3, 'C':4, 'Q':5, 'E':6, 'G':7, 'H':8, 
+             'I':9, 'L':10, 'K':11, 'M':12, 'F':13, 'P':14, 'S':15, 'T':16,
+             'W':17, 'Y':18, 'V':19}
+    return [aanumdict.get(res, 20) for res in seq]
+
+def loadfasta(fasta_file="test_seqs.fas"):
+    '''
+    Assumes a fasta file containing a single sequence
+    '''
+    
+    seqs=[]
+    with open(fasta_file,"r") as f:
+        for line in f:
+            line=line.rstrip("\n")
+            if ">" in line:
+                name=line
+            else:
+                seqs.append(line)
+    seqs=''.join(seqs)
+    iseqs=aas2int(seqs)
+    data=[name,iseqs,seqs]
+    return data 
